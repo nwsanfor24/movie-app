eaf30872d596cdbd32325b32c27b7ff9ef3d9585
Nathan Sanford <nwsanfor24@gmail.com>
2021-02-11 18:29:46 -0500

    api stuff inserted

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+const API_URL = 'https//api.moviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=566e12dd49b5daf1d3eb9aaa84737fa2&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https//api.moviedb.org/3/search/movie?api_key=566e12dd49b5daf1d3eb9aaa84737fa2&query="'
 
